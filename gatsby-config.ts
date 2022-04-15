27f1322812b084a877c6191c026e80d4ec1058c8
Mike Bamford <mb2174@cam.ac.uk>
2022-04-15 18:47:22 +0100

    Configured Gatsby with support for: - YAML content - image content - various useful libraries

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,25 @@
 import type { GatsbyConfig } from "gatsby"
+import path from "path"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `new`,
+    title: `Gatsby News Feed`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: path.resolve(`data`),
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-yaml",
+    "gatsby-transformer-sharp",
+  ],
 }
 
 export default config

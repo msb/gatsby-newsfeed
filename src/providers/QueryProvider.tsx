412174a093f9e5fb717c432761c5ac638cbd84d2
Mike Bamford <mike@msb.me.uk>
2022-04-22 09:45:28 +0100

    Created a context provider and hook for the current query string.

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+
+type Query = {
+  query: string
+  setQuery: (query: string) => void
+}
+
+// React context for the current index query.
+const QueryContext = React.createContext<Query>({query: "", setQuery: () => null})
+
+// Context provider for the current query text.
+const QueryProvider: FC = ({children}) => {
+
+  const [query, setQuery] = React.useState("")
+
+  return <QueryContext.Provider value={{query, setQuery}}>
+    { children }
+  </QueryContext.Provider>
+}
+
+export default QueryProvider
+
+export const useQuery = () => React.useContext(QueryContext)

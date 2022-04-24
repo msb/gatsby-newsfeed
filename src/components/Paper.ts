e475cf331be8325daa14dcecadbc64c8c7ee40d3
Mike Bamford <mike@msb.me.uk>
2022-04-24 21:06:03 +0100

    created a paper component

---
+++ src/components/Paper.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+// Simple component encapsulating styles for a paper component.
+const Paper = styled.div`
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.87);
+  background-color: #fff;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%);
+  padding: 8px;
+`
+
+export default Paper

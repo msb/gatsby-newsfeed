25c66feafe567680e387544555eff3ff2c8866a5
Mike Bamford <mb2174@cam.ac.uk>
2022-04-25 22:10:26 +0100

    f

--- src/components/content/ImageObject.tsx
+++ src/components/content/ImageObject.tsx
@@ -1,8 +1,8 @@
 import React, { FC, ReactElement } from 'react'
-import { IGatsbyImageData  } from 'gatsby-plugin-image';
-import Picture from '../Picture';
-import TwoPanelLayout from './TwoPanelLayout';
-import MainImage from './MainImage';
+import { IGatsbyImageData  } from 'gatsby-plugin-image'
+import { Picture } from '..'
+import TwoPanelLayout from './TwoPanelLayout'
+import MainImage from './MainImage'
 
 export type ImageObjectProps = {
   // The content's title
@@ -18,20 +18,13 @@ export type ImageObjectProps = {
 }
 
 // A component that renders image object content.
-const ImageObject: FC<ImageObjectProps> = ({ body, image: { childImageSharp }, title }): ReactElement => {
-  const { gatsbyImageData: {images, width} } = childImageSharp
-
-  if (images.sources === undefined) {
-    // sources appears never to be `undefined` (maybe poor typing?) so no need
-    // to handle this gracefully.
-    return <></>
-  }
-
-  return <TwoPanelLayout title={title} body={body} leftWidthRatio={0.65} >
+const ImageObject: FC<ImageObjectProps> = (
+  { body, image: { childImageSharp: { gatsbyImageData: { images, width } } }, title }
+): ReactElement => (<TwoPanelLayout title={title} body={body} leftWidthRatio={0.65} >
     <Picture sources={images.sources}>
       <MainImage decoding="async" {...images.fallback} alt={title} width={width} />
     </Picture>
   </TwoPanelLayout>
-}
+)
 
 export default ImageObject
--- src/components/content/Recipe.tsx
+++ src/components/content/Recipe.tsx
@@ -1,8 +1,8 @@
 import React, { FC, ReactElement } from 'react'
-import { IGatsbyImageData  } from 'gatsby-plugin-image';
-import Picture from '../Picture';
-import TwoPanelLayout from './TwoPanelLayout';
-import MainImage from './MainImage';
+import { IGatsbyImageData  } from 'gatsby-plugin-image'
+import { Picture } from '..'
+import TwoPanelLayout from './TwoPanelLayout'
+import MainImage from './MainImage'
 
 export type RecipeProps = {
   // The content's title
@@ -18,20 +18,14 @@ export type RecipeProps = {
 }
 
 // A component that renders recipe content.
-const Recipe: FC<RecipeProps> = ({ body, image: { childImageSharp }, title }): ReactElement => {
-  const { gatsbyImageData: {images, width} } = childImageSharp
-
-  if (images.sources === undefined) {
-    // sources appears never to be `undefined` (maybe poor typing?) so no need
-    // to handle this gracefully.
-    return <></>
-  }
-
-  return <TwoPanelLayout title={title} body={body} >
+const Recipe: FC<RecipeProps> = (
+  { body, image: { childImageSharp: { gatsbyImageData: { images, width } } }, title }
+): ReactElement => (
+  <TwoPanelLayout title={title} body={body} >
     <Picture sources={images.sources}>
       <MainImage decoding="async" {...images.fallback} alt={title} width={width} />
     </Picture>
   </TwoPanelLayout>
-}
+)
 
 export default Recipe

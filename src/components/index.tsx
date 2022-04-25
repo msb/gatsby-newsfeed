eaddbbc00720ce868312af3eeaaf3626cf282a32
Mike Bamford <mb2174@cam.ac.uk>
2022-04-25 22:01:24 +0100

    Simplification: the index page now directly uses <PortholeLink>

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,18 +1,8 @@
-import React from 'react'
-import Layout from './Layout'
 import Icon from './Icon'
-
+import Layout from './Layout'
+import Paper from './Paper'
+import Picture from './Picture'
 import PortholeLink, { LinkProperties } from './PortholeLink'
-import * as content from './content'
-
-// This a map page links keyed on content components.
-const pageLinks:{ [id: string] : React.FC<LinkProperties> } = {}
-
-// For now we only have one page link.
-for (const component in content) {
-    pageLinks[component] = PortholeLink
-}
 
 export type { LinkProperties }
-
-export { pageLinks, Layout, Icon }
+export { Icon, Layout, PortholeLink, Paper, Picture }

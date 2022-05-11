965826a1b8cfddbb03083c167ec7968011495e55
Mike Bamford <mb2174@cam.ac.uk>
2022-05-11 14:02:49 +0100

    missing css comment end marker causes chrome to hang

--- src/components/content/Table.tsx
+++ src/components/content/Table.tsx
@@ -24,7 +24,7 @@ const Container = styled.div<ContainerProps>`
 
   @media (max-width: ${breakWidth}px) {
     border: 0;
-    /* this is the max size of a one column table
+    /* this is the max size of a one column table */
     max-width: 800px;
   }
 `

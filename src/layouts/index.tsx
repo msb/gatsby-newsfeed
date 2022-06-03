bc53ebc86b254f44a9706cd87238e37082d59f57
Mike Bamford <mike@msb.me.uk>
2022-06-03 17:03:32 +0100

    Registered gatsby <Link> with <MDXProvider> so the import isn't required in MDX files.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,18 @@
 import React, { ReactElement, ReactNode } from 'react';
+import { MDXProvider } from '@mdx-js/react'
+import { Link } from 'gatsby'
 import QueryProvider from '../providers/QueryProvider';
 
 // This layout is used by "gatsby-plugin-layout" and exists purely to persist context over
 // different pages.
 function ContextLayout({ children }: { children: ReactNode }): ReactElement {
-  return <QueryProvider>{children}</QueryProvider>
+  return (
+    <MDXProvider components={{ Link }}>
+      <QueryProvider>
+        {children}
+      </QueryProvider>
+    </MDXProvider>
+  )
 }
 
 export default ContextLayout
